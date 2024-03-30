9f0d0b0dff5aad289875a29de96d9e2c52bf9f7f
thang2603 <thangc2k53@gmail.com>
2024-03-30 16:30:38 +0700

    test2

--- src/components/phat/index.jsx
+++ src/components/phat/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Phat = () => {
-  const { scene } = useLoader(GLTFLoader, "../models/phat1.glb");
+  const { scene } = useLoader(GLTFLoader, "/models/phat1.glb");
   return <group>{scene && <primitive object={scene} />}</group>;
 };
 
